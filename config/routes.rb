b99a9b1a1affe2d54c228e23791d44ac824128b2
onomeh <onwuanibeonome@gmail.com>
2023-02-22 13:10:16 +0000

    Configure route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/user' => "categories#index", :as => :user_root
 
   # Defines the root path route ("/")
   # root "articles#index"
