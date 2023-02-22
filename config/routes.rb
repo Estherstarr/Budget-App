6f40e5322e87013a2a14f7150b400725481f12c5
onomeh <onwuanibeonome@gmail.com>
2023-02-22 12:45:19 +0000

    Generate scaffold category

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
+  resources :categories
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
