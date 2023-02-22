aa306014b37d22b313ace2f92e87c8bfe2f3455a
onomeh <onwuanibeonome@gmail.com>
2023-02-22 18:17:32 +0000

    Create budget scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :budgets
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   resources :categories
